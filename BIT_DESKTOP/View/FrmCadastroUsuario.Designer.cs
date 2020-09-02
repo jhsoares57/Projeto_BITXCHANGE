9bbc71a9aac07980d8f00da2355ba61002d39fc9
LVKIN <backuplucas6150@gmail.com>
2020-09-02 17:51:34 -0300

    Adicionando insert.

--- BIT_DESKTOP/View/FrmCadastroUsuario.Designer.cs
+++ BIT_DESKTOP/View/FrmCadastroUsuario.Designer.cs
@@ -28,12 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnCadastrar = new MaterialSkin.Controls.MaterialRaisedButton();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtCpf = new System.Windows.Forms.TextBox();
+            this.txtNascimento = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.txtSexo = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnCadastrar.Depth = 0;
+            this.btnCadastrar.Location = new System.Drawing.Point(333, 272);
+            this.btnCadastrar.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Primary = true;
+            this.btnCadastrar.Size = new System.Drawing.Size(125, 48);
+            this.btnCadastrar.TabIndex = 0;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(333, 28);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(125, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // txtCpf
+            // 
+            this.txtCpf.Location = new System.Drawing.Point(333, 64);
+            this.txtCpf.Name = "txtCpf";
+            this.txtCpf.Size = new System.Drawing.Size(125, 20);
+            this.txtCpf.TabIndex = 1;
+            this.txtCpf.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtNascimento
+            // 
+            this.txtNascimento.Location = new System.Drawing.Point(333, 100);
+            this.txtNascimento.Name = "txtNascimento";
+            this.txtNascimento.Size = new System.Drawing.Size(125, 20);
+            this.txtNascimento.TabIndex = 1;
+            this.txtNascimento.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(333, 143);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(125, 20);
+            this.txtEmail.TabIndex = 1;
+            this.txtEmail.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(333, 189);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(125, 20);
+            this.txtSenha.TabIndex = 1;
+            // 
+            // txtSexo
+            // 
+            this.txtSexo.Location = new System.Drawing.Point(333, 233);
+            this.txtSexo.Name = "txtSexo";
+            this.txtSexo.Size = new System.Drawing.Size(125, 20);
+            this.txtSexo.TabIndex = 1;
+            // 
+            // FrmCadastroUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtSexo);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNascimento);
+            this.Controls.Add(this.txtCpf);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.btnCadastrar);
+            this.Name = "FrmCadastroUsuario";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private MaterialSkin.Controls.MaterialRaisedButton btnCadastrar;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtCpf;
+        private System.Windows.Forms.TextBox txtNascimento;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.TextBox txtSexo;
     }
 }
--- BIT_DESKTOP/View/FrmCadastroUsuario.cs
+++ BIT_DESKTOP/View/FrmCadastroUsuario.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -18,6 +19,35 @@ namespace BIT_DESKTOP.View
         public FrmCadastroUsuario()
         {
             InitializeComponent();
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            string strConn = ""; // STRING DE CONEXAO
+            string query = "INSERT INTO TB_USUARIO (" + txtNome.Text + "," + txtCpf.Text + "," + txtNascimento.Text + "," + txtEmail.Text + "," + txtSenha.Text + "," + txtSexo.Text + ") VALUES (@USU_STR_NOME, @USU_STR_CPF, @USU_DT_DTNASCIMENTO, @USU_STR_EMAIL, @USU_STR_SENHA, @USU_STR_SEXO)";
+            SqlConnection con = new SqlConnection(strConn);
+            SqlCommand sqlCMD = new SqlCommand(query, con);
+
+            con.Open();
+
+            sqlCMD.ExecuteNonQuery();
+
+            con.Close();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
