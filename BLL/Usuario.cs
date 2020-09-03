a1b39a9e8d3ad5ba02e735afa462d314c9595f7c
LVKIN <backuplucas6150@gmail.com>
2020-09-03 15:45:09 -0300

    update, select e string de conexão.

--- BLL/Usuario.cs
+++ BLL/Usuario.cs
@@ -38,5 +38,48 @@ namespace BLL
                 throw ex;
             }
         }
+
+        public void alterarUsuario(string nome, string email, string senha, string cpf, string sexo)
+        {
+            try
+            {
+                string query = "UPDATE TB_USUARIO SET " +
+                "USU_STR_NOME = NOME, USU_STR_EMAIL = EMAIL, USU_STR_SENHA = SENHA," +
+                "USU_STR_SEXO = SEXO" +
+                "WHERE USU_STR_CPF = CPF";
+                cf.Comando = cf.Conexao.CreateCommand();
+                cf.Comando.Parameters.AddWithValue("USU_STR_NOME", nome);
+                cf.Comando.Parameters.AddWithValue("USU_STR_EMAIL", email);
+                cf.Comando.Parameters.AddWithValue("USU_STR_SENHA", senha);
+                cf.Comando.Parameters.AddWithValue("USU_STR_CPF", cpf);
+                cf.Comando.Parameters.AddWithValue("USU_STR_SEXO", sexo);
+                cf.Comando.CommandType = CommandType.Text;
+                cf.Comando.CommandText = query.ToString();
+                cf.Conexao.Open();
+                SqlCommand sqlCommand = new SqlCommand(query, cf.Conexao);
+                cf.Conexao.Close();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public DbDataReader selecionaUsuario(string cpf)
+        {
+            try
+            {
+                string query = "SELECT * FROM TB_USUARIO WHERE USU_STR_CPF = CPF";
+                cf.Comando = cf.Conexao.CreateCommand();
+                cf.Comando.Parameters.AddWithValue("USU_STR_CPF", cpf);
+                cf.Comando.CommandType = CommandType.Text;
+                cf.Comando.CommandText = query.ToString();
+                return cf.Comando.ExecuteReader(query);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
     }
 }
