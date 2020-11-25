e004c0c55659aa2613238b4679e855c46f6b9fec
Hugo Barros <jhsoares57@gmail.com>
2020-11-24 22:29:05 -0300

    Ajuste na tela de recuperação de senha para validar se a senha e aconfirmação de senha são iguais. Ajuste para implententar botão de voltar a tela de login. Ajustado rota para tela de cadastro. Ajustado chamada do arquivo de layout.

---
+++ BIT_WEB/Scripts/Confirmar-AlteracaoSenha .js
@@ -0,0 +1,21 @@
+﻿var ValidaAlteracao = function (evento) {
+    senha_ = document.getElementById("nova-senha").value;
+    Confsenha_ = document.getElementById("conf-senha").value;
+    if (senha_ == Confsenha_) {
+        if (confirm("Confirma essa transação?")) {
+
+            return true;
+        }
+        else {
+            event.preventDefault();
+            return false;
+        }
+
+    }
+    else {
+        alert("Senhas não conferem!")
+            event.preventDefault();
+            return false;
+    }
+    
+}
--- BIT_WEB/Scripts/simular-conversao-home.js
+++ BIT_WEB/Scripts/simular-conversao-home.js
@@ -1,5 +1,4 @@
-﻿const fvalue = document.getElementById('fvalue')
-
+﻿
 function Calc() {
 
     valorreal = document.getElementById("real").value;
@@ -21,38 +20,4 @@ function Calc() {
     res_bit = Bit.replace(/,/g, ".");
     document.getElementById("BitCoin_Simu").innerHTML = (valorreal / res_bit);
 
-}
-
-function Fvalor() {
-
-    valorreal = document.getElementById("real").values.toFixed(2);
-
-    if (document.getElementsByName("type-fcoin").values == "real") {
-        //document.getElementById("fvalue").innerHTML = valorreal;
-        fvalue.value = valorreal.toFixed(2);
-
-    }//Dolar
-    else if (document.getElementByName("type-fcoin").values == "dolar") {
-
-        dolar = document.getElementById("CotDolar").innerText;
-        res_dolar = dolar.replace(/,/g, ".");
-        //document.getElementById("fvalue").innerHTML = (valorreal / res_dolar);
-        fvalue.value = (valorreal / res_dolar).toFixed(2);
-    }
-    //eth
-    else if (document.getElementByName("type-fcoin").values == "ethereum") {
-
-        ETH = document.getElementById("CotETH").innerText;
-        res_ETH = ETH.replace(/,/g, ".");
-        //document.getElementById("fvalue").innerHTML = (valorreal / res_ETH);
-        fvalue.value = (valorreal / res_ETH).toFixed(2);
-    }
-    else {
-        //Bit
-        //valorreal = document.getElementById("real").value;
-        Bit = document.getElementById("CotBit").innerText;
-        res_bit = Bit.replace(/,/g, ".");
-        //document.getElementById("fvalue").innerHTML = (valorreal / res_bit);
-        fvalue.value = (valorreal / res_bit).toFixed(2);
-    }
 }
